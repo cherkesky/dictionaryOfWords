f67b45f45bb745608fd8b17db2de492997b67003
Cherkesky <guy@cherkesky.com>
2020-01-13 14:53:26 -0600

    completed 1st part

---
+++ dictionaryOfWords.py
@@ -0,0 +1,29 @@
+"""
+Create a dictionary with key value pairs to
+represent words (key) and its definition (value)
+"""
+word_definitions = dict()
+
+"""
+Add several more words and their definitions
+   Example: word_definitions["Awesome"] = "The feeling of students when they are learning Python"
+"""
+
+
+word_definitions["damsel"] = "a young unmarried woman"
+word_definitions["svelte"] = "slender and elegant"
+word_definitions["bifurcation"] = "the division of something into two branches or parts"
+
+
+"""
+Use square bracket lookup to get the definition of two
+words and output them to the console with `print()`
+"""
+
+
+"""
+Loop over the dictionary to get the following output:
+    The definition of [WORD] is [DEFINITION]
+    The definition of [WORD] is [DEFINITION]
+    The definition of [WORD] is [DEFINITION]
+"""
